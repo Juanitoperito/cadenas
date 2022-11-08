0564e1744d57089e1e1b87aae7873cefef08780b
Juanitoperito <jperort689@g.educaand.es>
2022-11-08 11:58:34 +0100

    segundo ejercicio

---
+++ ejercicio2.py
@@ -0,0 +1,12 @@
+'''fruta es una variable de tipo cadena
+creariamos una variable
+fruta = "banana"
+luego imprimos fruta[:]
+Lo que hace el programa es obtener todos los caracteres de la palabra banana.
+Si pusiesemos fruta[2:] cogeria desde la segunda posicion hasta el final, saldria por pantalla nana
+fruta = "banana"
+print(fruta[:2])
+Otro caso seria
+fruta = "banana'
+print(fruta[:5])
+Saldria por pantalla banan, lo que hace es coger las letras del inicio hasta la posicion 5 la cual no esta incluida'''
